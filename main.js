f1f5992c8aa77779085cb1a7fa043c678f8a96ea
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-04 10:23:33 +0200

    changed text

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ let amountDue = 245.60;
 let discount = 0.05;
 let newAmount = (amountDue - (amountDue * discount).toFixed(2));
 alert('Amount due: R' + amountDue);
-alert('You qualify for a discount of 5% (' + discount + ') \n' +'Your new price to pay is: R' + newAmount);
+alert('You qualify for a discount of 5% \n ' + 'New amount due: R' + newAmount);
