f80703cfb367dd29d165174719ebfb387680bd2a
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-04 10:18:01 +0200

    fixed newAmount variable

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let amountDue = 245.60;
 let discount = 0.05;
-let newAmount = (amountDue * discount).toFixed(2);
+let newAmount = (amountDue - (amountDue * discount).toFixed(2));
 alert('Amount due: R' + amountDue);
-alert('You qualify for a discount of 5% (' + discount + ') \n' +'Your new price to pay is: R' + (amountDue - (amountDue*discount).toFixed(2)));
+alert('You qualify for a discount of 5% (' + discount + ') \n' +'Your new price to pay is: R' + newAmount);
